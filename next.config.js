24c45e092ba90297325823074cb532ddb70bc4fc
Jkrant87 <jonashenrykrant@gmail.com>
2026-04-13 17:12:40 -0700

    fix: clean next.config.js and vercel.json before first deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  experimental: {
-    mdxRs: true,
-  },
   images: {
-    formats: ['image/webp', 'image/avif'],
+    formats: ["image/webp", "image/avif"],
   },
 };
-
 module.exports = nextConfig;
